7dd347ba1a71a54fd79433fb2a9bd647a88ab08e
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2021-06-17 22:24:39 +0200

    Make bullet not take n as a param.

--- Sources/KeyCode/View/BulletList.swift
+++ Sources/KeyCode/View/BulletList.swift
@@ -11,16 +11,17 @@ public struct BulletList<Bullet: View, Content: View>: View {
 	public typealias Builder = BulletBuilder<Bullet>
 	
 	public var bullet: Builder.Function
-	@ViewBuilder public var content: (Builder) -> Content
+	@ViewBuilder public var content: (inout Builder) -> Content
 	
-	public init(_ bullet: @escaping Builder.Function, @ViewBuilder content: @escaping (Builder) -> Content) {
+	public init(_ bullet: @escaping Builder.Function, @ViewBuilder content: @escaping (inout Builder) -> Content) {
 		self.bullet = bullet
 		self.content = content
 	}
 	
 	public var body: some View {
 		VStack(alignment: .leading) {
-			content(BulletBuilder(bullet: bullet))
+			var builder = BulletBuilder(bullet: bullet)
+			content(&builder)
 		}
 	}
 }
@@ -29,10 +30,13 @@ public struct BulletBuilder<Bullet: View> {
 	public typealias Function = (Int) -> Bullet
 	
 	public var bullet: Function
+	public var n = 0
 	
-	public func callAsFunction<Content: View>(_ n: Int, @ViewBuilder content: () -> Content) -> some View {
-		HStack(alignment: .top, spacing: nil) {
-			bullet(n)
+	public mutating func callAsFunction<Content: View>(@ViewBuilder content: () -> Content) -> some View {
+		let currentN = n
+		n += 1
+		return HStack(alignment: .top, spacing: nil) {
+			bullet(currentN)
 			
 			VStack(alignment: .leading) {
 				content()
@@ -46,17 +50,22 @@ public extension BulletBuilder where Bullet == Text {
 		Text("•")
 	}
 	
-	static func numbered(_ n: Int, suffix: String = ".") -> some View {
-		Text("\(n + 1)\(suffix)")
+	static func numbered(_ n: Int) -> some View {
+		let suffix = "." // TODO Make configurable
+		return Text("\(n + 1)\(suffix)")
 	}
 }
 
-//struct BulletList_Previews: PreviewProvider {
-//    static var previews: some View {
-//		BulletList.numbered { _ in
-//			Text("Test 1 that is kinda long").frame(width: 70)
-//
-//			Text("Test 2")
-//		}
-//    }
-//}
+struct BulletList_Previews: PreviewProvider {
+    static var previews: some View {
+		BulletList(BulletBuilder.numbered) { bullet in
+			bullet {
+				Text("Test 1 that is kinda long").frame(width: 70)
+			}
+
+			bullet {
+				Text("Test 2")
+			}
+		}
+    }
+}
