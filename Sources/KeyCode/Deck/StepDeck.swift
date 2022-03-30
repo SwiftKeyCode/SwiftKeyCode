057303bfce678f122611596fb7a31a0f2c24f86f
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2022-03-30 23:10:56 +0200

    Fix StepDeck function.

--- Sources/KeyCode/Deck/StepDeck.swift
+++ Sources/KeyCode/Deck/StepDeck.swift
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-public struct StepDeck: AnyDeck {
-	public var states: [AnyDeck]
+public struct StepDeck<V: View>: AnyDeck {
+	public var states: [V]
 	
-	public init(_ range: Range<Int>, @DeckBuilder<AnyView> fun: (Int) -> AnyDeck) {
-		states = range.map(fun)
+	public init(_ range: Range<Int>, @DeckBuilder<AnyView> fun: (Int) -> [V]) {
+		states = range.flatMap(fun)
 	}
 	
 	public var steps: [AnyView] {
-		return states.flatMap { $0.steps }
+		return states.map { AnyView($0) }
 	}
 }
