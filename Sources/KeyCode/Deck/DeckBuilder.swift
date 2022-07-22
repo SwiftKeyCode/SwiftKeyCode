fb824a6487b103236acac49dca468af09bf06462
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2022-07-22 11:45:00 +0200

    Update a few comments.

--- Sources/KeyCode/Deck/DeckBuilder.swift
+++ Sources/KeyCode/Deck/DeckBuilder.swift
@@ -22,13 +22,18 @@ public extension DeckBuilder {
 	// Unfortunately, these functions interfere with the AnyView functions - the compiler can never decide
 	// which to use. We can't add these functions because the AnyView functions must be included. Otherwise,
 	// it would be up to the user to wrap his views with AnyView - which would be annoying.
+	// Update: It seems they are never used, currently.
 	
-//	static func buildBlock<C0: Deck>(_ c0: C0, _ c1: C0) -> [C] where C0.Content == C {
-//		[c0, c1].flatMap { $0.steps }
+//	static func buildBlock<C0: Deck, C1: Deck>(_ c0: C0, _ c1: C1) -> [C] where C0.Content == C, C1.Content == C {
+//		[c0.steps, c1.steps].flatMap { $0 }
 //	}
 //
-//	static func buildBlock<C0: Deck>(_ c0: C0, _ c1: C0, _ c2: C0, _ c3: C0) -> [C] where C0.Content == C {
-//		[c0, c1, c2, c3].flatMap { $0.steps }
+//	static func buildBlock<C0: Deck, C1: Deck, C2: Deck>(_ c0: C0, _ c1: C1, _ c2: C2) -> [C] where C0.Content == C, C1.Content == C, C2.Content == C {
+//		[c0.steps, c1.steps, c2.steps].flatMap { $0 }
+//	}
+//
+//	static func buildBlock<C0: Deck, C1: Deck, C2: Deck, C3: Deck>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3) -> [C] where C0.Content == C, C1.Content == C, C2.Content == C, C3.Content == C {
+//		[c0.steps, c1.steps, c2.steps, c3.steps].flatMap { $0 }
 //	}
 }
 
